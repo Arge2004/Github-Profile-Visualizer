3e75dba651a92cc7a1ec26648bd9a43b4bff92b3
Arge2004 <argenismedina0424@gmail.com>
2025-07-22 19:59:53 -0500

    cambios mobile

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -11,7 +11,7 @@ export default function Main() {
     return (
       <>
       <Header></Header>
-      <div className="flex text-lg rounded-2xl font-bold bg-secondary-color w-4/5 mx-auto h-[200px] flex-col items-center justify-center mt-60 shadow-2xl">
+      <div className="flex text-center lg:text-lg rounded-2xl font-bold bg-secondary-color w-fit lg:w-4/5 mx-auto p-8 flex-col items-center justify-center lg:mt-60 mt-30 shadow-2xl">
         <p className="text-primary-font">User not found. Please try again.</p>
       </div>
       </>
@@ -29,7 +29,7 @@ export default function Main() {
         <ProjectsSection repositories={repositories ?? []} ></ProjectsSection>
       </main>
       :
-      <div className="flex text-lg rounded-2xl font-bold bg-secondary-color w-4/5 mx-auto h-[200px] flex-col items-center justify-center mt-60 shadow-2xl">
+      <div className="flex text-center lg:text-lg rounded-2xl font-bold bg-secondary-color w-fit lg:w-4/5 sm:mx-auto mx-10 p-8 flex-col items-center justify-center lg:mt-60 mt-30 shadow-2xl">
         <p className="text-primary-font">Please search for a user to display their github profile.</p>
         <p className="text-primary-font">Example: "Arge2004"</p>
       </div>
