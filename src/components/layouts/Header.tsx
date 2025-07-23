9cb2ea8cf2e37ebf74f9768b01741e0b3f101b2e
Arge2004 <argenismedina0424@gmail.com>
2025-07-22 19:48:19 -0500

    cambios en el header base

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,20 @@
 import SearchInput from "../common/SearchInput";
+import ProfileImg from "../common/ProfileImg";
+import { useUser } from "../../context/UserContext";
 
 
 export default function Header() {
+  const {user,} = useUser();
   return (
-    <header className="bg-[url('public/background.jpg')] h-[240px] bg-cover min-[1250px]:bg-contain bg-center pt-8 ">
+    <div className="relative">
+    <header className="bg-[url('/background.jpg')] h-[240px] bg-cover min-[1250px]:bg-contain bg-center pt-8 ">
         <SearchInput></SearchInput>
     </header>
+    { !user && 
+    <div className="absolute -translate-y-1/2 left-6 sm:left-12 max-w-fit">
+        <ProfileImg></ProfileImg>
+    </div>
+   }
+    </div>
   )
 }
